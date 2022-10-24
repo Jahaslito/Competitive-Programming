fe44ee7620f2f59194beee8951a370f54e7a372e
Jahaslito <jahaslito24@gmail.com>
2022-10-24 07:52:43 +0300

    Improved FizzBuzz

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,16 +3,12 @@ class Solution:
         output = []
         for i in range(1, n+1):
             if i % 3 == 0 and i % 5 == 0:
-               # print("FizzBuzz")
                 output.append("FizzBuzz")
             elif i % 3 == 0:
-               # print("Fizz")
                 output.append("Fizz")
             elif i % 5 == 0:
-               # print("Buzz")
                 output.append("Buzz")
             else :
                 output.append(str(i))
-               # print(i)
         return output
         
