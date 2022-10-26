a9ed064dadb7157f51cd7e17c804d2fbd41bb68d
Jahaslito <jahaslito24@gmail.com>
2022-10-26 07:08:56 +0300

    Formatting

--- Week 1/SelectionSort.py
+++ Week 1/SelectionSort.py
@@ -10,9 +10,9 @@ class Solution:
         for i in range(n - 1):
             min = i
             for j in range(i,n):
-                if(arr[j]<arr[min]):
+                if(arr[j] < arr[min]):
                     min = j
-            arr[min],arr[i]=arr[i],arr[min]
+            arr[min], arr[i] = arr[i], arr[min]
         return arr
 
 
